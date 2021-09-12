434cf49e21fc6f3a018c20a8b483f9738f0c5ff0
TheWolek <pawelm916@wp.pl>
2021-09-12 20:41:21 +0200

    slider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.scss';
 
 import Banner from './components/Banner/Banner';
+import Slider from './components/Slider/Slider';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <Slider />
     </div>
   );
 }
