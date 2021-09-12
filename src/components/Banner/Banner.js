bf21033ba3176b037f97286829796227fd6c8087
TheWolek <pawelm916@wp.pl>
2021-09-12 20:26:14 +0200

    logo change

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,4 +1,4 @@
-import logo from '../../assets/logo.png'
+import logo from '../../assets/logo.svg'
 import accountImg from '../../assets/account.png'
 import basketImg from '../../assets/basket.png'
 
